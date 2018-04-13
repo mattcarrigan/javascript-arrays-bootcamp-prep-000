cfe8147468c95691f579e7d956a9748ff51255a3
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-04-13 12:10:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array,element) {
   [element, ...array];
-  return array;
 }
 addElementToBeginningOfArray ([1], 'foo');
 
